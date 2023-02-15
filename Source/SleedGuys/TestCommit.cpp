b40fe6e9d0fa3dff3667d8605d8cab48ad4a6c1d
George Apostolou <gamedev@sleed.gr>
2023-02-15 15:21:46 +0200

    Added Test Commit Class

---
+++ Source/SleedGuys/TestCommit.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestCommit.h"
+
+// Sets default values
+ATestCommit::ATestCommit()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATestCommit::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestCommit::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
