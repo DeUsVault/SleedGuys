a412a7018315a668d96c2236287b35f32cd3d5aa
George Apostolou <gamedev@sleed.gr>
2023-02-16 14:07:34 +0200

    Create Cpp Anim Instance, Create Blueprint Anim Instance and set idle/running animation

---
+++ Source/SleedGuys/Character/SleedAnimInstance.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SleedAnimInstance.h"
+#include "SleedCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void USleedAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	SleedCharacter = Cast<ASleedCharacter>(TryGetPawnOwner());
+}
+
+void USleedAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (SleedCharacter == nullptr)
+	{
+		SleedCharacter = Cast<ASleedCharacter>(TryGetPawnOwner());
+	}
+	if (SleedCharacter == nullptr) return;
+
+	FVector Velocity = SleedCharacter->GetVelocity();
+	Velocity.Z = 0.f;
+	Speed = Velocity.Size();
+
+	bIsInAir = SleedCharacter->GetCharacterMovement()->IsFalling();
+	bIsAccelerating = SleedCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f ? true : false;
+}
+
---
+++ Source/SleedGuys/Character/SleedAnimInstance.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SleedAnimInstance.generated.h"
+
+class ASleedCharacter;
+
+/**
+ * 
+ */
+UCLASS()
+class SLEEDGUYS_API USleedAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+private:
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
+	ASleedCharacter* SleedCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
+	
+};
