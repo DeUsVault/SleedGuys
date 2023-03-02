651ea7750f00098a393ef487baef651764d86592
George Apostolou <gamedev@sleed.gr>
2023-03-02 15:39:11 +0200

    Stamina Pickup/Regen create mechanism for regenerating stamina over time

--- Source/SleedGuys/SleederComponents/BuffComponent.cpp
+++ Source/SleedGuys/SleederComponents/BuffComponent.cpp
@@ -17,19 +17,41 @@ void UBuffComponent::BeginPlay()
 void UBuffComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	StaminaRampUp(DeltaTime);
+}
+
+void UBuffComponent::StaminaRampUp(float DeltaTime)
+{
+	if (!bIsStaminaRegenerating || Character == nullptr) return;
+
+	const float RegenThisFrame = StaminaRegeneratingRate * DeltaTime;
+	float AmountToRegen = FMath::Clamp(Character->GetStamina() + RegenThisFrame, 0.f, Character->GetMaxStamina());
+
+	Character->SetStamina(AmountToRegen);
+	Character->UpdateHUDStamina();
+	StaminaAmountToRegen = StaminaAmountToRegen - RegenThisFrame;
+
+	if (StaminaAmountToRegen <= 0.f || Character->GetStamina() >= Character->GetMaxStamina())
+	{
+		bIsStaminaRegenerating = false;
+		StaminaAmountToRegen = 0.f;
+	}
 }
 
 void UBuffComponent::AddStamina(float StaminaRegenAmount, float StaminaRegenTime, bool bRegenOverTime)
 {
-	if (!bRegenOverTime)
+	if (bRegenOverTime)
 	{	
-		float StaminaNum = FMath::Clamp(Character->GetStamina() + StaminaRegenAmount, 0.f, Character->GetMaxStamina());
-		Character->SetStamina(StaminaNum);
-		Character->UpdateHUDStamina(); // here it only happens if we have authority because on pickup class we set the overlapping functions to happen on authority only
+		bIsStaminaRegenerating = true;
+		StaminaRegeneratingRate = StaminaRegenAmount / StaminaRegenTime;
+		StaminaAmountToRegen = StaminaAmountToRegen + StaminaRegenAmount;
 	}
 	else
 	{
-
+		float StaminaNum = FMath::Clamp(Character->GetStamina() + StaminaRegenAmount, 0.f, Character->GetMaxStamina());
+		Character->SetStamina(StaminaNum);
+		Character->UpdateHUDStamina(); // here it only happens if we have authority because on pickup class we set the overlapping functions to happen on authority only
 	}
 }
 
--- Source/SleedGuys/SleederComponents/BuffComponent.h
+++ Source/SleedGuys/SleederComponents/BuffComponent.h
@@ -22,6 +22,7 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+	void StaminaRampUp(float DeltaTime);
 
 private:
 	UPROPERTY()
